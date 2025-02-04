738e5f78680fe1373e119663f8c5afbad6fce5a1
Kirill Denisenko <kirill.denisenko@oracle.com>
2025-02-04 02:32:20 +0000

    First iteration of F1 ex

--- F1.py
+++ F1.py
@@ -3,3 +3,15 @@
 # Versionn: 1.0
 # Description: F1
 
+import math
+
+no_laps = input ("Number of laps: ")
+
+no_laps= int(no_laps)
+fuel_consumption = 2.25
+fuel_req = no_laps * fuel_consumption
+
+print ("The minimum fuel requirement for", str(no_laps),"laps is",str(fuel_req),"kg.")
+
+
+
