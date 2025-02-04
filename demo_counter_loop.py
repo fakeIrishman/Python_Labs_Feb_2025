8ae94d7c4d973bf41cf2bf83f23fcb72225203bf
Kirill Denisenko <kirill.denisenko@oracle.com>
2025-02-04 20:19:34 +0000

    Initial version demo_counter_loop.py

---
+++ demo_counter_loop.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# Author: kdenisen
+# Version: 
+# Description:
+"""
+   Docstring:
+"""
+
+print ("while loop:s")
+count = 0
+while count < 10:
+    print (count)
+    count += 1
+
+print ("for loop:")
+
+for num in range(0, 10, 1):
+    print (num)
+
+for num in range(0, 10):
+    print(num)
+
+
+for num in range(10):
+        print(num)
+
+
