78a83bab827287f51add4cd75a4a11acd26f08db
Kirill Denisenko <kirill.denisenko@oracle.com>
2025-02-04 19:44:55 +0000

    Adds else clause for while loop to message on too many attempts to demo_pin.py

--- demo_pin.py
+++ demo_pin.py
@@ -17,7 +17,11 @@ while pin != master_pin and attempts < 3:
     pin = input ("Enter PIN: ")
     if pin == master_pin:
         print ("Valid PIN")
+        break
     else:
         print ("Invalid PIN")
         attempts += 1
+else:
+    # Executes ONLY ONCE when LOOP becomes False
+    print ("Too many attempts, you card will be shredded")
 print ("Done")
