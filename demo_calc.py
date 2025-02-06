67ba99da8a2e6e03a4f06c5d0253a7a2a3ee5cc9
Kirill Denisenko <kirill.denisenko@oracle.com>
2025-02-06 16:09:23 +0000

    demo functions

---
+++ demo_calc.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# Author: kdenisen
+# Version: 
+# Description: Simulates calculator app
+"""
+   Docstring: Calculator App with add, mult and div functions
+"""
+def add (x,z):
+    return float(x+z)
+
+def mul (x, z):
+    return float(x*z)
+
+def div (x, z ):
+    return round(x/z,3)
